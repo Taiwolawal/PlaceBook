9d49e30b7183169ae7897c18945b08b009f8909c
Lawal Taiwo <taiwolawal360@gmail.com>
2021-01-12 18:02:14 +0100

    Creation of database, repository, viewmodel and addition of kotlin coroutines dependency for background thread.

--- app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.raywenderlich.placebook.R
+import com.raywenderlich.placebook.ui.MapsActivity
 
 // 1
 class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter {
@@ -31,7 +32,7 @@ class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter
         val phoneView = contents.findViewById<TextView>(R.id.phone)
         phoneView.text = marker.snippet ?: ""
         val imageView = contents.findViewById<ImageView>(R.id.photo)
-        imageView.setImageBitmap(marker.tag as Bitmap?)
+        imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
         return contents
     }
 }
